dc61657b6ba3312aeace013a64055f3cb2e4b0bc
Sashanya-alt <81250858+Sashanya-alt@users.noreply.github.com>
2021-08-16 08:52:42 +0300

    Add files via upload

---
+++ tets2.cpp
@@ -0,0 +1,11 @@
+﻿// tets2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
